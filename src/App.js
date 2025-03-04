41206554d4230c48ce3119a21968aeb932715ba9
sanjay17s <sanjay.17suresh2000@gmail.com>
2025-03-04 23:14:38 +0530

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-
-const App = () => {
-  
-  const [currentPage,setCurrentPage] = useState('home');
-  
-  const showAboutPage = () => (
-    setCurrentPage('about')
-  )
-
-  const showHomePage = () => (
-    setCurrentPage('home')
-  )
-  if(currentPage === 'home') {
-   return (
+function HomePage() {
+  return (
     <div>
-      <h1>Home </h1>
-      <button onClick={showAboutPage}>ABOUT Page</button>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is the main content of our website.</p>
     </div>
-   )
-  }
-  else {
-    return (
-      <div>
-      <h1>About </h1>
-      <button onClick={showHomePage}>Home Page</button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Learn more about our company and mission.</p>
     </div>
+  );
+}
 
-    )
-  }
-  
+function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Get in touch with our team.</p>
+    </div>
+  );
+}
 
+function NotFoundPage() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you are looking for doesn't exist.</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home </Link>
+        <Link to="/about">About </Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
